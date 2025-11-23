e17fc49a69218684903242a8131e3fa105281489
Pere Viader <pere@viader.cat>
2025-11-23 23:25:20 +0100

    feat: Remove aspire and update docker compose

--- MySmartHome.AppHost/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿var builder = DistributedApplication.CreateBuilder(args);
-
-var automations = builder.AddProject<Projects.MySmartHome_Automations>("automations")
-    .WithExternalHttpEndpoints();
-
-builder.AddProject<Projects.MySmartHome_Web>("web")
-    .WithExternalHttpEndpoints();
-
-builder.Build().Run();
